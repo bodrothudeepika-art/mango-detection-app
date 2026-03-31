05aca4f36a06522d4c8a669bc38150e71499546d
bodrothudeepika-art <bodrothu.deepika@gmail.com>
2026-04-01 01:24:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 import streamlit as st
 from PIL import Image
+from ultralytics import YOLO
+import tempfile
 
 st.title("Mango Tree Counter 🌳🥭")
+
+model = YOLO("best.pt")
 
 uploaded_file = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    
+    # Save temp file
+    temp_file = tempfile.NamedTemporaryFile(delete=False)
+    temp_file.write(uploaded_file.read())
+
+    # Run YOLO
+    results = model(temp_file.name)
+
+    # Show result
+    result_img = results[0].plot()
+    st.image(result_img, caption="Detection Result", use_column_width=True)
